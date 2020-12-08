f5104a5820ae559e62fa21e6877347e2d5ae7595
Jakeas <JakeASierra@gmail.com>
2020-12-08 15:50:03 -0500

    first commit

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
+function copy(arr){
     /*your code here*/
 }    
-
+console.log("final", copy(originalFlavors))
 
 
 
